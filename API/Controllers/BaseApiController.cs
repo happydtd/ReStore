f67c16bc54d5d404a658c4ce6cdc650f53de2209
happydtd <happydtdau@yahoo.com.au>
2022-05-24 09:02:09 +1000

    add basket

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
@@ -12,6 +6,6 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-
+        
     }
 }
---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BasketController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket(){
+            var basket = await _context.Baskets
+            .Include(i=>i.Items)
+            .ThenInclude(p=>p.Product)
+            .FirstOrDefaultAsync(x=>x.BuyerId == Request.Cookies["buyerId"]);
+        }
+    }
+}
