6ba5641df357b7c1b5e3788948005f25539acc7f
happydtd <happydtdau@yahoo.com.au>
2022-05-26 23:44:52 +1000

    add pagination

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -22,12 +25,26 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(){
-            return await _context.Products.ToListAsync();
+        //[FromQuery]希望从url传值，而不是body
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
+        {
+            //return await _context.Products.ToListAsync();
+            var query = _context.Products
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
+            .AsQueryable();
+
+            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.MetaData);
+            return products;
+            //return await query.ToListAsync();
         }
 
         [HttpGet("{id}")] //api/products/3
-        public async Task<ActionResult<Product>> GetProduct(int id){
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
             var product = await _context.Products.FindAsync(id);
             if (product == null)
                 return NotFound();
